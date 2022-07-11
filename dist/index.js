28581950e43df6cc8de86f1c61215dc7625c31e0
kevinbathrick-kr <kevin.bathrick@kroger.com>
2022-07-11 15:17:22 +0000

    Build app

--- dist/index.js
+++ dist/index.js
@@ -5145,10 +5145,6 @@ async function run() {
     const token = core.getInput("github-token", { required: true });
     const octokit = github.getOctokit(token);
     const labelNames = await getPullRequestLabelNames(octokit);
-    //     const labels = getInputLabels();
-    //     const result = labels.every(
-    //         (label) => labelNames.findIndex((value) => label === value) >= 0
-    //     );
     core.setOutput("result", labelNames);
 }
 async function getPullRequestLabelNames(octokit) {
@@ -5163,11 +5159,6 @@ async function getPullRequestLabelNames(octokit) {
     const pr = response.data.length > 0 && response.data[0];
     return pr ? pr.labels.map((label) => label.name || "") : [];
 }
-// function getInputLabels(): string[] {
-//     const raw = core.getInput("labels", { required: true });
-//     const json = JSON.parse(raw);
-//     return Array.isArray(json) ? json : [];
-// }
 run().catch((err) => {
     core.setFailed(err.message);
 });
